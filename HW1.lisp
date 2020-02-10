21f07088132859e489c919a6e984eb08b8400ecc
shoham shitrit <shohami.sh@gmail.com>
2020-02-10 00:02:42 -0500

    finished functionality and readme

--- HW1.lisp
+++ HW1.lisp
@@ -1,4 +1,4 @@
-(defun has-keyword(l x)
+(defun has-keyword(l x) ;checking if a list has atom x
 	(if (eq l nil)
 		nil
 		(if(eq (car l) x)
@@ -8,60 +8,74 @@
 	)
 
 )
-(defun has-keywords (l x y)
-	(if (has-keyword l x)
-		(format t "~%has first one")  
-		(if (has-keyword l y)
-		(format t "~%has second one")
-		t)
-	)
-	nil
-)
+
 (defun name-greeting (input)
-	(format t "~%Hello, ~a! It's great to meet you." (last input)) ;assume last word is the name.
+	(format t "~%Hello, ~a! It's great to meet you." '(last input)) ;assume last word is the name.
 )
-(defun write-response (input)
-    (format t "~{~a~^~}" input)    
-    (cond
+(defun write-response (input) ; this function takes user input and tries to match it to the appropriate output that Robbie will say. 
+    (cond ;has combinations of comparing to full sentences and checking if a certain keyword exists. 
        ((equalp input (list 'Are 'you 'a 'human? )) 
        (format t "~%Is this a real question? I am Robbie the ROBOT."))
 	   ((and (has-keyword input 'favorite) (has-keyword input 'color?)) (format t "~%My favorite color is black."))
 	   ( (equalp input (list 'What 'are 'your 'hobbies?)) (format t "~%I have no hobbies. I am a boring simple chatbot. All I do is talk."))
-       ((equalp (car input) 'bye)
-        nil)
+		((and (has-keyword input 'favorite) (has-keyword input 'sport?)) (format t "~%Favorite sport? Hm...Competitve Calculating."))
+		((and (has-keyword input 'favorite) (has-keyword input 'song?)) (format t "~%Robot Rock by Daft Punk. Do I even have to explain..."))
+		((and (has-keyword input 'favorite) (has-keyword input 'movie?)) (format t "~%Iron Man. I always imagine I'm Jarvis!"))
         ((has-keyword input 'from?) (format t "~%I am from Rochester, but I hate the snow."))
 		((has-keyword input 'name?) (format t "~%My name is Robbie. You knew that. Duh."))
-		((and (has-keyword input 'My) (has-keyword input 'is)) (name-greeting input))
-       (t (format t "~%I do not understand...Ask me something else."))
+		((and (has-keyword input 'My) (has-keyword input 'name)) (name-greeting input)) ;greeting specified by name
+		 ((equalp input (list 'Do 'you 'have 'friends? )) 
+		 (format t "~%Yes! Alexa and Siri are my favorite friends. I despise Cortana though, shes so annoying."))
+		((equalp (car input) 'bye)
+        nil) ;exit condition. Robbie will say nothing from these responses.
+       (t (format t "~%I do not understand...Ask me something else.")); deafult message for queries I did not implement.
     )
 )
 
 
 
 (defun conversation() ;question 1 
-    (format t "Conversation Time! Type \"help\" to see what I am capable of, or type \"bye\" to quit.Lets Go!~%")
+    (format t "Conversation Time! Type \"bye\" to quit. Remember to end your statements with a tilde.Lets Go!~%")
 	(format t "Robbie: Hello! I am Robbie the robot. Whats your name?")
     (loop
     (format t "~%You:") 
-	(setq input (read-delimited-list #\~))
+	(let input (read-delimited-list #\~)) ;read from input, delimit it, then send it to write-response
     (write-response input)
-	(when (equalp (car input) 'bye) (return (format t "~%Bye Bye, human!~%"))) ;check for q
+	(when (equalp (car input) 'bye) (return (format t "~%Bye Bye, human!~%"))) ;check for bye, then print farewell.
     ) 
 
 )
-;(conversation)
+(conversation) 
 
 
 ;question 2
+
+;; setting knowledge base.
 (defparameter *KB* (make-hash-table :test #'equal))
     (setf (gethash '(Robot Robbie) *KB*) T)
+	(setf (gethash '(Robot Alexa) *KB*) T)
+	(setf (gethash '(Robot Siri) *KB*) T)
     (setf (gethash '(Likes Robbie Samantha) *KB*) T)
+	(setf (gethash '(Likes Robbie Shoham) *KB*) T)
+	(setf (gethash '(Likes Robbie Romeo) *KB*) T)
+	(setf (gethash '(Likes Robbie Lebron) *KB*) T)
     (setf (gethash '(Dog Rover) *KB*) T)
     (setf (gethash '(Bark Rover) *KB*) T)
+	(setf (gethash '(Likes Robbie Juliet) *KB*) T)
+	(setf (gethash '(Owns Robbie Processor) *KB*) T)
+	(setf (gethash '(Owns Robbie Processor) *KB*) T)
+	(setf (gethash '(Movie Iron-Man) *KB*) T)
+	(setf (gethash '(Movie Avengers) *KB*) T)
+	(setf (gethash '(Movie Star-Wars) *KB*) T)
+	(setf (gethash '(Movie Parasite) *KB*) T)
 
-(defun has-predicate (wff)
+
+
+
+
+
+(defun has-predicate (wff) ;; for each key in KB, check if the first element is equal to the first element of wff.
 	(loop for k being each hash-key of *KB*
-
 		thereis (equalp (car k) (car wff)) 
 	)
 )
@@ -79,10 +93,10 @@
 (defun print-fun (f a) ;evaluate and print function "E, P"
 	(format t "(~a ~a) => ~a ~%" f a (funcall f a))
 )
-(defun repl1(f) ;repl for 1 arg
+(defun repl1(f) ;repl for part two. Enter elements in a list. Ex: (Robot Robbie)
 	(format t "Enter arguments for ~a (q to stop): " f)
 	(finish-output nil)
-	(setq arg1 (read))
+	(let arg1 (read))
 	(if (equalp (format nil "~a" arg1) "q") ;check for q
 		nil
 		(if (print-fun f arg1)
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # CSC291-A1
+Shoham Shitrit
+31391856
+sshitrit
+# For Part 1:
+Sample Statements Robbie Can Respond To:
+1.) Whats your name?
+2.) Whats your favorite movie? //accepts other questions w/ keywords favorite and movie
+3.) Whats your favorite song? //accepts other questions w/ keywords favorite and song
+4.) My name is ____ (Fill w/ your name)
+5.) What are your hobbies
+6.) Do you have friends?
+7.) What is your favorite color? //accepts other combinations w/ keyowrd color
+8.) Are you a human?
+
+Things to remember when inputting statements:
+When finishing a statement, end it by hitting space bar and then typing a tilde (~), then pressing enter 
+to see robbie's response.
+To quit and see robbie's farewell to you, type "bye ~". 
+## Structure: 
+Conversation method to run;
+    -> Calls (write-response) on user input
+        ->validates input based on keywords or full text
+            ->uses (has-keyword)
+            ->uses (name-greeting) for specialzied name greeting.
+
+# For Part 2:
+
+Things you can query in knowledge base-
+    1.)  Checking who is a robot (Robbie, Alexa, and Siri)
+    2.) Checking who likes each other. i.e. (Likes Robbie Shoham)
+    3.) Checking what is a movie (Iron Man, Avengers, Star-Wars)
+    4.) What Robbie Owns (processor)
+Enter items into the REPL as a list. For example:
+Enter arguments for ANSWER-YNQ (q to stop): (Robot Robbie) <- THIS IS HOW TO INPUT THE LIST
+    -> should return (ANSWER-YNQ (ROBOT ROBBIE)) => (ROBOT ROBBIE) 
+Enter arguments for ANSWER-YNQ (q to stop): (Dog Bill)
+(ANSWER-YNQ (DOG Bill)) => (NOT (DOG Bill))
+Enter arguments for ANSWER-YNQ (q to stop): (Bill)
+(ANSWER-YNQ (Bill)) => UNKNOWN
+## Structure
+    (repl1 'answer-ynq) ; construcutes a repl that repeatedly calls answer-ynq until one types 'q'.
+        ->calls (answer-ynq)
+            ->checks (gethash) 
+            ->(checks (has-predicate)
+        -> prints result using (print-fun)
